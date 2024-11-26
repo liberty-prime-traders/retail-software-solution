b19de4b43c3258fe3c6b7fa9db0975e13a9bc321
Ezra Orina <Ezra.Orina@nisc.coop>
2024-11-25 22:57:40 -0600

    1 - setup test endpoints with okta authorization

--- src/main/kotlin/me/ezra_home/retail_software_solution/RetailSoftwareSolutionApplication.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/RetailSoftwareSolutionApplication.kt
@@ -1,9 +1,10 @@
 package me.ezra_home.retail_software_solution
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class RetailSoftwareSolutionApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/me/ezra_home/retail_software_solution/security/CorsConfig.kt
@@ -0,0 +1,16 @@
+package me.ezra_home.retail_software_solution.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class CorsConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val originPatterns = System.getenv("CORS_PATTERNS").split(',').toTypedArray()
+        registry.addMapping("/**").allowedOrigins(*originPatterns)
+    }
+}
