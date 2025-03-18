1c9b6d8f5e20dd365609941b8a81b41bf5f10065
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-18 14:40:31 +0300

    Update FruitEndpoint.kt
    Updated fruitendpoint

--- src/main/kotlin/me/ezra_home/retail_software_solution/rest/endpoints/fruit/FruitEndpoint.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/rest/endpoints/fruit/FruitEndpoint.kt
@@ -3,7 +3,16 @@ package me.ezra_home.retail_software_solution.controller
 import me.ezra_home.retail_software_solution.model.dto.FruitRequestDTO
 import me.ezra_home.retail_software_solution.model.dto.FruitResponseDTO
 import me.ezra_home.retail_software_solution.service.FruitService
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
