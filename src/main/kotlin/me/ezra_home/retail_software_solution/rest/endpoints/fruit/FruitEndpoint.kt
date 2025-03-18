9f81d75274728fe7cc396482565aab8dfc188041
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-18 14:08:56 +0300

    Update FruitEndpoint.kt
    updated fruit endpoints

--- src/main/kotlin/me/ezra_home/retail_software_solution/rest/endpoints/fruit/FruitEndpoint.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/rest/endpoints/fruit/FruitEndpoint.kt
@@ -3,35 +3,34 @@ package me.ezra_home.retail_software_solution.controller
 import me.ezra_home.retail_software_solution.model.dto.FruitRequestDTO
 import me.ezra_home.retail_software_solution.model.dto.FruitResponseDTO
 import me.ezra_home.retail_software_solution.service.FruitService
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.UUID
 
 @RestController
-@RequestMapping("/api/fruits")
+@RequestMapping("secured/fruits")
 class FruitController(private val fruitService: FruitService) {
 
-    @GetMapping("/{id}")
-    fun getFruitById(@PathVariable id: UUID): ResponseEntity<FruitResponseDTO> {
-        return ResponseEntity.ok(fruitService.getFruitById(id))
+    @GetMapping("{id}")
+    fun getFruitById(@PathVariable id: UUID): FruitResponseDTO {
+        return fruitService.getFruitById(id)
     }
 
-    @GetMapping("/search")
-    fun getFruitByName(@RequestParam name: String): ResponseEntity<FruitResponseDTO> {
-        return ResponseEntity.ok(fruitService.getFruitByName(name))
+    @GetMapping("search")
+    fun getFruitByName(@RequestParam name: String): FruitResponseDTO {
+        return fruitService.getFruitByName(name)
     }
 
     @PostMapping
-    fun createFruit(@RequestBody request: FruitRequestDTO): ResponseEntity<FruitResponseDTO> {
-        return ResponseEntity.ok(fruitService.createFruit(request))
+    fun createFruit(@RequestBody request: FruitRequestDTO): FruitResponseDTO {
+        return fruitService.createFruit(request)
     }
 
-    @PutMapping("/{id}")
-    fun updateFruit(@PathVariable id: UUID, @RequestBody request: FruitRequestDTO): ResponseEntity<FruitResponseDTO> {
-        return ResponseEntity.ok(fruitService.updateFruit(id, request))
+    @PutMapping("{id}")
+    fun updateFruit(@PathVariable id: UUID, @RequestBody request: FruitRequestDTO): FruitResponseDTO {
+        return fruitService.updateFruit(id, request)
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     fun deleteFruit(@PathVariable id: UUID): ResponseEntity<Unit> {
         fruitService.deleteFruit(id)
         return ResponseEntity.noContent().build()
