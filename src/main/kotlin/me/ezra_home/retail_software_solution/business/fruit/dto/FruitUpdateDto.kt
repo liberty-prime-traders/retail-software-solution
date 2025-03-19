da214930520e6a98f3f4ab657067746e352e0af0
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-20 01:00:27 +0300

    updates done on RTSS_ 81

--- src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/dto/FruitUpdateDto.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/dto/FruitUpdateDto.kt
@@ -3,12 +3,14 @@ package me.ezra_home.retail_software_solution.fruit.dto
 import me.ezra_home.retail_software_solution.model.entity.FruitEntity
 import java.util.Locale
 import java.math.BigDecimal
+import java.util.UUID
 
 
 /**
  * DTO for {@link me.ezra_home.retail_software_solution.model.entity.FruitEntity}
  */
 data class FruitUpdateDto(
+    val id: UUID?,
     val name: String?,
     val alternateName: String?,
     val color: String?,
