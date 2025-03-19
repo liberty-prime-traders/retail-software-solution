2215e17400da6a7b9c62497cefb08ff3a74131c1
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-19 22:55:36 +0300

    Update FruitEntity.kt
    Fruity entity updated

--- src/main/kotlin/me/ezra_home/retail_software_solution/model/entity/FruitEntity.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/model/entity/FruitEntity.kt
@@ -1,6 +1,8 @@
 package me.ezra_home.retail_software_solution.model.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import java.math.BigDecimal
 
 @Entity
