67bd840f7ce566e4f7e04f3073616bc640a223f3
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-20 20:01:23 +0300

    updates

--- src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/FruitMapper.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/FruitMapper.kt
@@ -10,16 +10,16 @@ import org.mapstruct.*
 @Mapper(config = RtsMapperConfig::class)
 interface FruitMapper {
 
-    @Mapping(source = "createdBy", target = "createdBy", qualifiedBy = [FullName::class])
+    @Mapping(source = "createdById", target = "createdBy", qualifiedBy = [FullName::class])
     fun toDto(fruitEntity: FruitEntity): FruitResponseDto
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdById", ignore = true)
     @Mapping(target = "createdOn", ignore = true)
     @BeanMapping(qualifiedBy = [CreatedBy::class])
     fun toEntity(fruitInsertDto: FruitInsertDto): FruitEntity
 
-    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdById", ignore = true)
     @Mapping(target = "createdOn", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     fun partialUpdate(fruitUpdateDto: FruitUpdateDto, @MappingTarget fruitEntity: FruitEntity)
