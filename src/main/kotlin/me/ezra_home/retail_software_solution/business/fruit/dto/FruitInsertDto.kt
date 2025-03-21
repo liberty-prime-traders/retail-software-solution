b258f2bc75cf9333eaa029d425e91a4ae18b3c80
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-21 14:04:04 +0300

    updates fruit dto

--- src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/dto/FruitInsertDto.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/dto/FruitInsertDto.kt
@@ -1,9 +1,6 @@
 package me.ezra_home.retail_software_solution.business.fruit.dto
 
-import me.ezra_home.retail_software_solution.model.entity.FruitEntity
 import java.math.BigDecimal
-import java.time.OffsetDateTime
-import java.util.UUID
 
 data class FruitInsertDto(
     val name: String,
