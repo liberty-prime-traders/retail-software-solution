b9af6e44d55d1452ae9d34cc7e64b4615c020ab3
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-23 20:54:29 +0300

    updates fruit RTSS 82

--- src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/FruitCache.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/FruitCache.kt
@@ -1,13 +1,13 @@
 package me.ezra_home.retail_software_solution.business.fruit
 
-import java.util.UUID
+import me.ezra_home.retail_software_solution.configuration.cache.CacheNames
 import me.ezra_home.retail_software_solution.model.entity.FruitEntity
-import me.ezra_home.retail_software_solution.configuration.cache.CacheNames  
 import me.ezra_home.retail_software_solution.repository.FruitRepository
 import org.springframework.cache.annotation.CacheConfig
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 @CacheConfig(cacheNames = [CacheNames.FRUIT])
@@ -17,7 +17,7 @@ class FruitCache(private val fruitRepository: FruitRepository) {
     fun getAllFruits(): Collection<FruitEntity> = fruitRepository.findAll()
 
     @CacheEvict(allEntries = true)
-    fun upsertFruit(fruitEntity: FruitEntity): FruitEntity = fruitRepository.save(fruitEntity)
+    fun upsertFruit(fruitEntity: FruitEntity) = fruitRepository.save(fruitEntity)
 
     @CacheEvict(allEntries = true)
     fun deleteFruit(id: UUID) {
