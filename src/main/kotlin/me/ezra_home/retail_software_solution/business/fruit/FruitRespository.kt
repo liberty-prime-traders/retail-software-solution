8422ade453055f6a822a789d8a44e9162e2feb5c
cox101 <104098884+cox101@users.noreply.github.com>
2025-03-24 23:44:17 +0300

    updates fruit RTSS 82

--- src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/FruitRespository.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/business/fruit/FruitRespository.kt
@@ -1,10 +1,9 @@
-package me.ezra_home.retail_software_solution.repository
+package me.ezra_home.retail_software_solution.business.fruit
 
 import me.ezra_home.retail_software_solution.model.entity.FruitEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.UUID
 
-@repository
-interface FruitRepository : JpaRepository<FruitEntity, UUID> {
-    fun findByName(name: String): FruitEntity?
-}
+@Repository
+interface FruitRepository : JpaRepository<FruitEntity, UUID>
