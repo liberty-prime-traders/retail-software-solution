fb65b2de91b8d7910bb5a33e964382fc678ef5c8
Ezra Orina <ezraorina53@gmail.com>
2025-04-29 01:13:34 -0500

    RTSS-102 made changes to accommodate client changes

--- src/main/kotlin/me/ezra_home/retail_software_solution/platform/business/locationadmin/LocationAdminMapper.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/platform/business/locationadmin/LocationAdminMapper.kt
@@ -2,10 +2,13 @@ package me.ezra_home.retail_software_solution.platform.business.locationadmin
 
 import me.ezra_home.retail_software_solution.configuration.mapping.RtsMapperConfig
 import me.ezra_home.retail_software_solution.platform.model.LocationAdminEntity
+import me.ezra_home.retail_software_solution.util.business.mappers.userinfo.FullName
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper(config = RtsMapperConfig::class)
 interface LocationAdminMapper {
+    @Mapping(source = "adminId", target = "admin", qualifiedBy = [FullName::class])
     fun toResponseDto(locationAdminEntity: LocationAdminEntity): LocationAdminResponseDto
 
 }
