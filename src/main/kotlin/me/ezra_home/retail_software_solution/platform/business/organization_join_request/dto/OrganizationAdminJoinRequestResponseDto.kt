2f1543c73cd44dad426012a2cb4031ed6779bb08
AdongoJr2 <dev.mosesadongo@gmail.com>
2025-05-23 19:57:55 +0300

    Add ID to `OrganizationAdminJoinRequestResponseDto` and `OrganizationJoinRequestResponseDto`

--- src/main/kotlin/me/ezra_home/retail_software_solution/platform/business/organization_join_request/dto/OrganizationAdminJoinRequestResponseDto.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/platform/business/organization_join_request/dto/OrganizationAdminJoinRequestResponseDto.kt
@@ -3,8 +3,10 @@ package me.ezra_home.retail_software_solution.platform.business.organization_joi
 import me.ezra_home.retail_software_solution.util.enums.JoinRequestStatus
 import java.io.Serializable
 import java.time.OffsetDateTime
+import java.util.UUID
 
 data class OrganizationAdminJoinRequestResponseDto(
+    val id: UUID?,
     val fullName: String,
     val requestedDate: OffsetDateTime,
     val status: JoinRequestStatus
--- src/main/kotlin/me/ezra_home/retail_software_solution/platform/business/organization_join_request/dto/OrganizationJoinRequestResponseDto.kt
+++ src/main/kotlin/me/ezra_home/retail_software_solution/platform/business/organization_join_request/dto/OrganizationJoinRequestResponseDto.kt
@@ -3,8 +3,10 @@ package me.ezra_home.retail_software_solution.platform.business.organization_joi
 import me.ezra_home.retail_software_solution.util.enums.JoinRequestStatus
 import java.io.Serializable
 import java.time.OffsetDateTime
+import java.util.UUID
 
 data class OrganizationJoinRequestResponseDto(
+    val id: UUID?,
     val domain: String,
     val requestedDate: OffsetDateTime,
     val status: JoinRequestStatus
