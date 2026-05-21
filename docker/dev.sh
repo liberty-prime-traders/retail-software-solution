36ec250dbf640ba14cef8ed391499a71332d1651
Ezra Orina <ezraorina53@gmail.com>
2026-05-20 20:26:12 -0500

    RTS-202 push docker changes

--- docker/dev.sh
+++ docker/dev.sh
@@ -7,7 +7,9 @@ ENV_FILE="dev.env"
 if [[ "$ACTION" == "down" ]]; then
   docker compose -f postgres-compose.yml --env-file "$ENV_FILE" down --remove-orphans
   docker compose -f kafka-compose.yml --env-file "$ENV_FILE" down --remove-orphans
+  docker compose -f redis-compose.yml --env-file "$ENV_FILE" down --remove-orphans
 else
   docker compose -f postgres-compose.yml --env-file "$ENV_FILE" up -d
   docker compose -f kafka-compose.yml --env-file "$ENV_FILE" up -d
+  docker compose -f redis-compose.yml --env-file "$ENV_FILE" up -d
 fi
